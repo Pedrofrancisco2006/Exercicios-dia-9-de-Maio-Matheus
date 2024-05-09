0780aedb3e51b2e30df37df8e4ff1024c843711e
Pedro Vasco <166523038+Pedrofrancisco2006@users.noreply.github.com>
2024-05-09 09:40:47 -0300

    Add files via upload

---
+++ Numeros crescente.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite um numero: "))
+
+if n1 == n2 :
+    print("Os numeros são iguais")
+elif (n1 > n2):
+    print("O" ,n1, "é maior que" ,n2)    
+else:
+    print(n2, "é maior que", n1)    
---
+++ fatorial.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um numero: "))
+fatorial = 1
+for i in range(numero , 1 , -1):
+    fatorial *= i
+print(f"O fatorial de {numero} é {fatorial}")
